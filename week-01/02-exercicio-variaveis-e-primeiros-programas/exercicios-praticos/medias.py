f9b98908feb12b4183312922aecdc08c1f43a8c3
Rodrigo Borge <rodrigo@borge.com.br>
2023-12-30 20:48:40 -0300

    exercicios praticos

---
+++ week-01/02-exercicio-variaveis-e-primeiros-programas/exercicios-praticos/medias.py
@@ -0,0 +1,9 @@
+nota1 = int(input("Digite a primeira nota: "))
+nota2 = int(input("Digite a primeira nota: "))
+nota3 = int(input("Digite a primeira nota: "))
+nota4 = int(input("Digite a primeira nota: "))
+
+somaNotas = nota1 + nota2 + nota3 + nota4
+media = somaNotas / 4
+
+print("A média aritmética é ", media)
