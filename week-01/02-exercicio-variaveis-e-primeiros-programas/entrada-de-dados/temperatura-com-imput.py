2ec3eaeda442f30b2ae0c60bf5a4dc7a73e87270
Rodrigo Borge <rodrigo@borge.com.br>
2023-12-29 23:33:23 -0300

    exercicios modulo 02

---
+++ week-01/02-exercicio-variaveis-e-primeiros-programas/entrada-de-dados/temperatura-com-imput.py
@@ -0,0 +1,5 @@
+temperaturaFahrenheit = input("Digite um temperatura em Fahrenheit para conversão: ")
+
+temperaturaCelsius = (float(temperaturaFahrenheit) - 32) * 5/9
+
+print("A temperatura em celsius é: ",temperaturaCelsius)
---
+++ week-01/02-exercicio-variaveis-e-primeiros-programas/entrada-de-dados/temperatura.py
@@ -0,0 +1,5 @@
+temperaturaFahrenheit = 78
+
+temperaturaCelsius = (temperaturaFahrenheit - 32) * 5/9
+
+print("A temperatura em celsius é: ",temperaturaCelsius)
