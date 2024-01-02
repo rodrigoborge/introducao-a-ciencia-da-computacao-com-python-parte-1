a4176dc8980822339c99f5a1fc803f5aaad6f29d
Rodrigo Borge <rodrigo@borge.com.br>
2024-01-01 22:07:29 -0300

    week 4

---
+++ week-04/produto-sequencia.py
@@ -0,0 +1,11 @@
+tamanho = int(input("Digite o tamanho maximo da sequencia: "))
+
+produto = 1
+i = 0
+
+while i < tamanho:
+    valor = int(input("Digite um valor a ser multiplicado: "))
+    produto = produto * valor
+    i = i + 1
+    
+print ("O produto dos valores digitados é: ", produto)
---
+++ week-04/repeticao-while.py
@@ -0,0 +1,5 @@
+i = 0
+
+while i <= 64:
+    print (2 ** i)
+    i = i + 1
